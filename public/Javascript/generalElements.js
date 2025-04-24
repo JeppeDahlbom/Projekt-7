685a1130e38daab58a757508a86b4258292292df
Valdemar <pizza27@live.dk>
2025-04-24 10:05:32 +0200

    Fuck chatten

--- public/Javascript/generalElements.js
+++ public/Javascript/generalElements.js
@@ -1,15 +1,15 @@
 // Indlæs header
-fetch("/header.html")
-    .then(response => response.text())
-    .then(data => {
+fetch("HTML/Elements/header.html")
+  .then(response => response.text())
+  .then(data => {
     const header = document.querySelector("header");
     if (header) header.innerHTML = data;
-    });
+  });
 
 // Indlæs footer
-fetch("/../HTML/Elements/HTML/header.html")
-    .then(response => response.text())
-    .then(data => {
+fetch("HTML/Elements/footer.html")
+  .then(response => response.text())
+  .then(data => {
     const footer = document.querySelector("footer");
     if (footer) footer.innerHTML = data;
-    });
+  });
