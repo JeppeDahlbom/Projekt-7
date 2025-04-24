4029ebec8a18feca76eb793d3598f53e497446e0
Valdemar <pizza27@live.dk>
2025-04-24 12:22:41 +0200

    Vi elsker ikke vue

--- vue-app/src/components/Footer.vue
+++ vue-app/src/components/Footer.vue
@@ -1,4 +1,4 @@
 <template>
     <div><h1>Billetter</h1></div>
-</template>
+  </template>
   
--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,4 +1,12 @@
+<script setup>
+import Header from './../components/Header.vue'
+import Footer from './../components/Footer.vue'
+</script>
+
+
 <template>
+    <Header></Header>
     <div><h1>Forside</h1></div>
+    <Footer></Footer>
   </template>
   
