e5ddb62218ce8d2ef0d14080cebf5a946f55a706
JeppeDahlbom <jeppedahlbom@hotmail.com>
2025-05-01 09:44:29 +0200

    Update Season.vue

--- vue-app/src/views/Season.vue
+++ vue-app/src/views/Season.vue
@@ -6,45 +6,39 @@ import newsBG3 from '../assets/images/newsBG3.webp'
 const links = [
   {
     title: "VI RYKKER SAMMEN",
-    link: "Stå sammen på lægterne, når vi møder AC Horsens den 24. December kl. 21.30",
+    link: "/",
     image: playerBG
   },
   {
     title: "VI RYKKER SAMMEN",
-    link: "Stå sammen på lægterne, når vi møder AC Horsens den 24. December kl. 21.30",
+    link: "/",
     image: playerBG
   }
-]
+];
 
 </script>
 
 
 
 <template>
-    <div class="image-container">
-      <img src="../assets/images/newsBG3.webp" alt="News Background">
-    </div>
-<a v-for="element in links href="">
-    <div v-bind:style="{ backgroundImage: 'url(' + element.image + ')' }">
-            
-                <div class="backgroundCover" style="background-color: rgba(0, 0, 0, 0.5);">
+<a class="link" v-for="element in links" :href=" element.link ">
+    <div class="background" v-bind:style="{ backgroundImage: 'url(' + element.image + ')' }">
+                <div style="background-color: rgba(0, 0, 0, 0.1);">
                     <h3>{{ element.title }}</h3>
-                    <p>{{ element.link }}</p>
             </div>
         </div></a>
   </template>
   
   <style scoped>
-  .image-container {
+  .link,div{
     width: 100%;
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 20px;
-    box-sizing: border-box;
+    height: 100px;
   }
-  img {
-    width: 100%;
-    height: 200px;
-    display: block;
+  .background {
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  h3{
+    margin-top: auto;
   }
   </style>
