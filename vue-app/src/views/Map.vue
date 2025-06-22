11d428ef03e9eecc61035eabe952a7f09a849e5b
oliven1212 <60175607+oliven1212@users.noreply.github.com>
2025-06-22 23:14:02 +0200

    Update Map.vue

--- vue-app/src/views/Map.vue
+++ vue-app/src/views/Map.vue
@@ -103,9 +103,43 @@ onMounted(() => {
       <div class="dot"></div>
     </div>
   </div>
+  <h2 class="titleMark">Find sæde</h2>
+  <input type="text">
+  <button class="search"><a>Søg</a></button>
+
+  <div class="background">
+    <div class="container" >
+      <h3 class="liga">Super Liga</h3>
+      <h3 class="homeName">OB</h3> 
+      <h3 class="v">vs</h3>
+      <h3 class="awayName">EFB</h3>
+      <h3 class="timeDate" >Søn 20 Apr - 09:30</h3>
+      <img :src="`assets/icons/OBLogo.svg`" alt="" class="home logoImg">
+      <img :src="`assets/icons/EFBLogo.svg`" alt="" class="away logoImg">
+    </div>
+    <div class="buyBG">
+      <h3 class="teams">OB - EFB</h3>
+      <h3 class="dateLocation">Søndag 20. Apr - Nature energy park<br>Sæde A8 - 47</h3>
+      <h3 class="time">09:30</h3>
+      <router-link to="/Kort">Find plads</router-link>
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
+input{
+  border-width: 2px;
+  border-style: solid;
+  border-color: darkgray;
+  padding: 2px 6px;
+  margin: auto;
+  color: #0048FF;
+  font-family: "Anton", sans-serif;
+  font-style: normal;
+  transform:scaleY(1.1);
+  letter-spacing: 2.5px; 
+}
 #map {
   --positionDotSize: 16px;
   position: relative;
@@ -115,6 +149,7 @@ onMounted(() => {
   overflow: hidden;
   background-image: url('/assets/images/stadion.webp');
   background-size: contain;
+  background-repeat: no-repeat;
 }
 
 #position {
@@ -156,5 +191,197 @@ onMounted(() => {
   
 }
 
+.search{
+  border-radius: 8px;
+  border: none;
+  background-color: var(--Blue);
+  padding: 4px 12px;
+  margin-left: 10px;
+}
+
+
+
+
+
+
+
+
+
+  
+  
+  .container h3{
+    text-align: center;
+    text-transform: uppercase;
+    line-height: 1;
+    display: inline-block
+  }
+  
+
+  .category-buttons {
+    display: flex;
+    justify-content: space-around;
+    padding: 12px;
+    background-color: #f2f2f2;
+  }
+  
+  .category-buttons button {
+    flex: 1;
+    margin: 0 4px;
+    padding: 10px;
+    border: 1px solid #999;
+    background-color: white;
+    font-size: 14px;
+  }
+  
+  
+  .ticket-card {
+    background-color: #e6e6e6;
+    margin: 16px;
+    border: 1px solid #ccc;
+    overflow: hidden;
+  }
+  
+  .ticket-image {
+    background-color: #ccc;
+    height: 120px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-style: italic;
+    font-size: 16px;
+    text-align: center;
+  }
+  
+  .ticket-details {
+    padding: 12px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: relative;
+  }
+  
+  .buy-button {
+    align-self: flex-end;
+    margin-top: 8px;
+    padding: 6px 12px;
+    background-color: white;
+    border: 1px solid #999;
+    cursor: pointer;
+  }
+  .background{
+    padding: 0px 5px 5px 5px;
+    box-shadow: 0px 2px 3px 0.5px rgba(0, 0, 0, 0.235);
+    margin-bottom: 15px;
+    background-image: url('/assets/images/3FSuperliga.svg');
+    background-repeat: no-repeat;
+    background-position: 80% 85%;
+    background-size: 65%;
+
+  }
+
+  .background .container{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-areas:
+      "home liga away"
+      "home homeName away"
+      "home v away"
+      "home awayName away"
+      "home dateTime away";
+      
+    margin: 10px; /* juster som ønsket */
+    background-image: url('/assets/images/ticketBG.svg');
+    background-repeat: no-repeat;
+    background-position: top;
+    background-size: 100%;
+    padding-bottom: 10px;
+    margin-bottom: 0;
+  }
+  h3.timeDate{
+    grid-area: dateTime;
+    font-size: 12px;
+   text-align: center;
+   margin-top: 5px;
+  }
+  .home{
+    grid-area: home;
+  }
+  .away{
+    grid-area: away;
+  }
+  h3.liga{
+    grid-area: liga;
+    font-size: 8px;
+    background-color: white;
+    color: var(--Blue);
+    width: max-content;
+    margin: auto;
+    margin-top: 0;
+    padding: 5px 7px;
+    border-radius: 0px 0px 10px 10px;
+    margin-bottom: 5px;
+  }
+  h3.awayName{
+    grid-area: awayName;
+    font-size: 18px;
+  }
+  h3.homeName{
+    grid-area: homeName;
+    font-size: 18px;
+  }
+
+  h3.v{
+    grid-area: v;
+    font-size: 12px;
+    margin: 7px auto;
+    
+  }
+  .logoImg{
+    margin: auto;
+    width: 50%;
+
+  }
+  .buyBG {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  flex-wrap: wrap;
+  position: relative;
+  margin: 10px;
+  margin-top: 0;
+  margin-bottom: 10px;
+  }
+  .buyBG h2 {
+    font-size: 16px;
+    flex-basis: 100%;
+    color: black;
+  }
+
+  .buyBG a {
+    background-color: var(--Blue); 
+    color: white;
+    padding: 4px 8px; 
+    border-radius: 8px;
+    margin-left: auto;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
+  .buyBG h3 {
+    color: black;
+    flex-basis: 100%;
+
+  }
+  .buyBG .teams{
+    font-size: 25px;
+  }
+  .buyBG .dateLocation{
+    font-size: 15px;
+
+  }
+  .buyBG .time{
+    font-size: 15px;
+
+  }
 
 </style>
